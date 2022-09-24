b828334881ffa9230b5d451b4611c49b81d811a6
katsue-watanabe <k-losmama@docomo.ne.jp>
2022-09-24 20:26:04 +0900

    Initial setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    root "users/sessions#new"
+    get '/sign_in', to: 'users/sessions#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
