5e76f67d98e260ccced004a1fe9699963283019f
katsue-watanabe <k-losmama@docomo.ne.jp>
2022-10-07 22:59:00 +0900

    image

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
- 
+
   validates :title, presence: true, length: { maximum: 10 }
   validates :content, presence: true
 
-  def articles
-    Article.where(article_id: self.id)
-  end
+  mount_uploader :image, ImageUploader 
 end
 
